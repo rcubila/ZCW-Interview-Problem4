40795694b3c53bc649de55a490ed30bf1e792888
raul <raul@zipcoders-MacBook-Pro.local>
2017-11-27 15:40:15 -0500

    raul

--- src/main/java/io/zipcoder/Problem4.java
+++ src/main/java/io/zipcoder/Problem4.java
@@ -1,4 +1,25 @@
 package io.zipcoder;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Problem4 {
+
+    public static void main(String[] args) {
+
+        Problem4 problem4 = new Problem4();
+        System.out.println(problem4.permutePalindrome("cd cdc dcdeeeef "));
+
+    }
+
+    public boolean permutePalindrome(String word) {
+
+        word = word.replaceAll("\\s+", "").trim();
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < word.length(); i++) {
+            if (!set.add(word.charAt(i))) set.remove(word.charAt(i));
+        }
+        return set.size() <= 1;
+    }
+
 }
--- src/test/java/io/zipcoder/Problem4Test.java
+++ src/test/java/io/zipcoder/Problem4Test.java
@@ -1,4 +1,24 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem4Test {
+
+    Problem4 problem4 = new Problem4();
+    @Test
+    public void permutePalindromeTest(){
+
+        boolean expected = true;
+        boolean actual = problem4.permutePalindrome("aab");
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void permutePalindromeTest2(){
+        boolean expected = true;
+        boolean actual = problem4.permutePalindrome("a    a  b   ");
+        Assert.assertEquals(expected, actual);
+    }
+
+
 }
